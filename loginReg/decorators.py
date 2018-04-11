14c8627cc52dd69036c248957581f49a128b2563
rds0751 <rds0751@gmail.com>
2018-04-12 03:40:44 +0530

    decorator else condition

--- loginReg/decorators.py
+++ loginReg/decorators.py
@@ -1,9 +1,12 @@
 from loginReg.models import User
+from django.core.exceptions import PermissionDenied
 
 def is_Executive(function):
     def wrap(request, *args, **kwargs):
         if request.User.rio == "Executive":
             return function(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
     return wrap
@@ -12,14 +15,18 @@ def is_Admin(function):
     def wrap(request, *args, **kwargs):
         if request.User.rio == "Admin":
             return function(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
     return wrap
 
-def is_Student(function):
+def is_Applicant(function):
     def wrap(request, *args, **kwargs):
         if request.User.rio == "Student":
             return function(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
     return wrap
