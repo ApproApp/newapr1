5e252856fbc73c16c37f7539b6ba4964a3bbd7a0
rahulk98 <rahul.krishnan27@gmail.com>
2018-04-15 15:37:32 +0530

    registration redefined

--- loginReg/models.py
+++ loginReg/models.py
@@ -1,19 +1,19 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 # Create your models and register that model in the admin section
-'''class User(models.Model):
-    username = models.OneToOneField(User, on_delete=models.PROTECT,)
+class User(AbstractUser):
     email = models.EmailField(max_length=100, default='')
     oid = models.CharField(max_length=20, default='')
     rio = models.CharField(max_length=20, default='')
-    password1 = models.CharField(max_length=20, default='')
-
-
-def create_profile(sender,**kwargs):
-    if kwargs['created']:
-        user = User.objects.create(user=kwargs['instance'])
-
-
-post_save.connect(create_profile, sender=User)'''
+#
+# def create_profile(sender,**kwargs):
+#     if kwargs['created']:
+#         user = User.objects.create(user=kwargs['instance'])
+#
+#
+# post_save.connect(create_profile, sender=User)
+    def __str__(self):
+        return self.username
